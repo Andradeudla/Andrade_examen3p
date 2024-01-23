cd2b94c6745394108b145a2780d981a4cc4c2179
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2024-01-23 14:59:37 -0500

    quinto commit

--- listado_alumons.c
+++ listado_alumons.c
@@ -28,20 +28,20 @@ int main()
         switch (opcion)
         {
         case 1:                                     // em caso que el usuario elige la opcion 1 se realizara lo siguiente
-            printf("Ingrese matricula: ");          // pide al usuario la matricula del estudiante
+            printf("Ingrese numero de matricula: ");          // pide al usuario la matricula del estudiante
             scanf("%d", &estudiantes[i].matricula); // lee la matricula del estudiante
             fflush(stdin);                          // limpia el buffer de entrada
 
             printf("Ingrese nombre: ");  // pide al usuario el nombre del estudiante
             gets(estudiantes[i].nombre); // lee el nombre del estudiante
 
-            printf("Ingrese direccion: ");  // pide al usuario la dirección del estudiante
+            printf("Ingrese direccion (ingrese el sector donde vive): ");  // pide al usuario la dirección del estudiante
             gets(estudiantes[i].direccion); // lee la direccion del estudiante
 
             printf("Ingrese carrera: ");  // pide al usuario la carrera del estudiante
             gets(estudiantes[i].carrera); // lee la carrera del estudiante
 
-            printf("Ingrese promedio: ");          // pide al usuario el promedio del estudiante
+            printf("Ingrese promedio (puede incluir decimales∫): ");          // pide al usuario el promedio del estudiante
             scanf("%f", &estudiantes[i].promedio); // lee el promedio del estudiante
             fflush(stdin);                         // limpia el buffer de entrada
 
@@ -52,7 +52,7 @@ int main()
             for (int j = 0; j < i; j++) // bucle que recorre todos los estudiantes ingresados
             {
                 printf("-------------Listado de Alumnos----------- \n");
-                printf("\nEstudiante %d:\n", j + 1);                 // imprime el encabezado del estudiante
+                printf("\nEstudiante %d\n", j + 1);                 // imprime el encabezado del estudiante
                 printf("Matricula: %d\n", estudiantes[j].matricula); // imprime la matricula del estudiante
                 printf("Nombre: ");                                  // imprime el nombre del estudiante
                 puts(estudiantes[j].nombre);
@@ -64,7 +64,7 @@ int main()
             }
             break; // termina el caso 2
         case 3:    // caso en el que el usuario elige la opcion 3
-            printf("Saliendo del programa :).......");
+            printf("Saliendo del programa :)....... \n");
             exit(0); // termina el programa
 
         default:                                             // Caso en el que el usuario elige una opcion no valida
