94ea7daf934c5bca1bcd05dfe0fbc5fb68ebb649
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2024-01-23 14:22:19 -0500

    tercer commit

--- listado_alumons.c
+++ listado_alumons.c
@@ -27,11 +27,43 @@ int main()
 
         switch (opcion)
         {
-        case 1: // em caso que el usuario elige la opcion 1 se realizara lo siguiente
-        
-            
+        case 1:                                     // em caso que el usuario elige la opcion 1 se realizara lo siguiente
+            printf("Ingrese matricula: ");          // pide al usuario la matricula del estudiante
+            scanf("%d", &estudiantes[i].matricula); // lee la matricula del estudiante
+            fflush(stdin);                          // limpia el buffer de entrada
+
+            printf("Ingrese nombre: ");  // pide al usuario el nombre del estudiante
+            gets(estudiantes[i].nombre); // lee el nombre del estudiante
+
+            printf("Ingrese direccion: ");  // pide al usuario la dirección del estudiante
+            gets(estudiantes[i].direccion); // lee la direccion del estudiante
+
+            printf("Ingrese carrera: ");  // pide al usuario la carrera del estudiante
+            gets(estudiantes[i].carrera); // lee la carrera del estudiante
+
+            printf("Ingrese promedio: ");          // pide al usuario el promedio del estudiante
+            scanf("%f", &estudiantes[i].promedio); // lee el promedio del estudiante
+            fflush(stdin);                         // limpia el buffer de entrada
+
+            i++; // incrementa el contador de estudiantes
+
             break;
-        
+        case 2:                         // caso en el que el usuario elige la opcion 2
+            for (int j = 0; j < i; j++) // bucle que recorre todos los estudiantes ingresados
+            {
+                printf("-------------Listado de Alumnos----------- \n");
+                printf("\nEstudiante %d:\n", j + 1);                 // imprime el encabezado del estudiante
+                printf("Matricula: %d\n", estudiantes[j].matricula); // imprime la matricula del estudiante
+                printf("Nombre: ");                                  // imprime el nombre del estudiante
+                puts(estudiantes[j].nombre);
+                printf("Direccion: "); // imprime la direccion del estudiante
+                puts(estudiantes[j].direccion);
+                printf("Carrera: "); // imprime la carrera del estudiante
+                puts(estudiantes[j].carrera);
+                printf("Promedio: %.2f\n", estudiantes[j].promedio); // imprime el promedio del estudiante
+            }
+            break; // termina el caso 2∫
+
         default:
             break;
         }
